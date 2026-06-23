5d3a47785e48df1df8a51c897e63a07537ad117d
Jyoti-Gupta <jg765495@gmail.com>
2026-06-23 10:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (66.77%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -2,33 +2,35 @@ class Solution {
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
 
-        int leftH = getLeft(root);   
-        int rightH = getRight(root); 
+    //     int leftH = getLeft(root);   
+    //     int rightH = getRight(root); 
 
-        if (leftH == rightH) {
+    //     if (leftH == rightH) {
             
-            return (1 << leftH) - 1; // 2^leftH - 1
-        }
+    //         return (1 << leftH) - 1; // 2^leftH - 1
+    //     }
 
       
-        return 1 + countNodes(root.left) + countNodes(root.right);
-    }
+    //     return 1 + countNodes(root.left) + countNodes(root.right);
+    // }
 
-    private int getLeft(TreeNode node) {
-        int h = 0 ;
-        while (node != null) {
-            h++;
-            node = node.left;
-        }
-        return h;
-    }
+    // private int getLeft(TreeNode node) {
+    //     int h = 0 ;
+    //     while (node != null) {
+    //         h++;
+    //         node = node.left;
+    //     }
+    //     return h;
+    // }
 
-    private int getRight(TreeNode node) {
-        int h = 0;
-        while (node != null) {
-            h++;
-            node = node.right;
-        }
-        return h;
+    // private int getRight(TreeNode node) {
+    //     int h = 0;
+    //     while (node != null) {
+    //         h++;
+    //         node = node.right;
+    //     }
+    //     return h;
+
+    return countNodes(root.left)+countNodes(root.right)+1;
     }
 }
